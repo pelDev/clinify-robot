03b29d92f54035e217f991259d9d4a2473055369
pelDev <pelumidevelops18@gmail.com>
2022-12-06 05:14:29 -0500

    deployment update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,8 +5,9 @@ import { LoggingInterceptor } from './interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  await app.listen(process.env.NODE_ENV === 'production' ? 80 : 4000);
 }
 bootstrap();
